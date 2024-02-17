6eb54433a72e0db5adc9026e54fa7181fc2de2c8
Lari <larihid1@gmail.com>
2024-02-16 21:22:33 -0600

    fixed buttons

--- paint.py
+++ paint.py
@@ -9,7 +9,7 @@ customtkinter.set_default_color_theme("dark-blue")
 root = customtkinter.CTk()
 root.title("Paint")
 root.configure(fg_color="#FFFFFF")
-root.geometry('750x600')
+root.geometry('600x600')
 
 
 drawing_mode = None
@@ -19,7 +19,7 @@ radius = 0
 temp_image = None
 
 
-canvas_image = np.ones((750, 600, 4), dtype=np.uint8) * 255  
+canvas_image = np.ones((600, 600, 4), dtype=np.uint8) * 255  
 cv2.circle(canvas_image, (0, 0), 1, (0, 0, 0, 0), -1)  
 
 def on_button_click(mode):
@@ -38,9 +38,9 @@ def on_canvas_drag(event):
         temp_image = canvas_image.copy()
         if drawing_mode == "line":
             temp_image = canvas_image.copy()
-            cv2.line(temp_image, (start_x, start_y), (end_x, end_y), (255, 0, 0, 255), 2) 
+            cv2.line(temp_image, (start_x, start_y), (end_x, end_y), (255, 0, 0, 255), 2)  
         elif drawing_mode == "freehand":
-            cv2.line(canvas_image, (start_x, start_y), (end_x, end_y), (0, 0, 255, 255), 2) 
+            cv2.line(canvas_image, (start_x, start_y), (end_x, end_y), (0, 0, 255, 255), 2)  
             start_x, start_y = end_x, end_y
         elif drawing_mode == "circle":
             temp_image = canvas_image.copy()
@@ -49,11 +49,11 @@ def on_canvas_drag(event):
         elif drawing_mode == "square":
             cv2.rectangle(temp_image, (start_x, start_y), (end_x, end_y), (0,255, 0, 255), 2)
             print('mover')
-            
         elif drawing_mode == "erase":
             temp_image = canvas_image.copy()
             radius = int(np.sqrt((end_x - start_x) ** 2 + (end_y - start_y) ** 2))
             cv2.circle(temp_image, (start_x, start_y), radius, (255, 255, 255, 0), -1)  
+       
         update_canvas(temp_image)
 
 def on_canvas_release(event):
@@ -63,25 +63,19 @@ def on_canvas_release(event):
         end_x, end_y = event.x, event.y
        
         if drawing_mode == "line":
-          
-            cv2.line(canvas_image, (start_x, start_y), (end_x, end_y), (255, 0, 0, 255), 2) 
+            cv2.line(canvas_image, (start_x, start_y), (end_x, end_y), (255, 0, 0, 255), 2)  
         elif drawing_mode == "freehand":
-            cv2.line(canvas_image, (start_x, start_y), (end_x, end_y), (0, 0, 255, 255), 2) 
+            cv2.line(canvas_image, (start_x, start_y), (end_x, end_y), (0, 0, 255, 255), 2)  
             start_x, start_y = end_x, end_y
         elif drawing_mode == "circle":
-            
             radius = int(np.sqrt((end_x - start_x) ** 2 + (end_y - start_y) ** 2))
             cv2.circle(canvas_image, (start_x, start_y), radius, (0, 255, 0, 255), 2)  
-        
         elif drawing_mode == "square":
             cv2.rectangle(canvas_image, (start_x, start_y), (end_x, end_y), (0,255, 0, 255), 2)
             print('mover')
-            
         elif drawing_mode == "erase":
-            
             radius = int(np.sqrt((end_x - start_x) ** 2 + (end_y - start_y) ** 2))
             cv2.circle(canvas_image, (start_x, start_y), radius, (255, 255, 255, 0), -1)  
-
 
     update_canvas()
 
@@ -93,9 +87,14 @@ def update_canvas(temp_image=None):
     canvas.itemconfig(canvas.canvas_item, image=photo)
     canvas.tk_img = photo
 
-frame_buttons = customtkinter.CTkFrame(root, bg="white")
-frame_buttons.place(x=10, y=10)
 
+canvas = customtkinter.CTkCanvas(root, bg="white", width=600, height=600)
+canvas.place(x=0, y=0)
+canvas.tk_img = ImageTk.PhotoImage(image=Image.fromarray(cv2.cvtColor(canvas_image, cv2.COLOR_BGRA2RGBA)))
+canvas.canvas_item = canvas.create_image(0, 0, anchor="nw", image=canvas.tk_img)
+canvas.bind("<Button-1>", on_canvas_click)
+canvas.bind("<B1-Motion>", on_canvas_drag)
+canvas.bind("<ButtonRelease-1>", on_canvas_release) 
 
 buttons = [
     {"mode": "line", "image_path": "Paint/images/LineaRecta.jpeg", "text": "Línea Recta"},
@@ -105,18 +104,12 @@ buttons = [
     {"mode": "erase", "image_path": "Paint/images/Borrador.jpeg", "text": "Borrar"}
 ]
 
+button_widgets = []
 for i, button_info in enumerate(buttons):
     img = customtkinter.CTkImage(light_image=Image.open(button_info["image_path"]), size=(50, 50))
-    button = customtkinter.CTkButton(frame_buttons, image=img, fg_color="#FFFFFF", text=button_info["text"], text_color="#000", hover_color="#FFF")
-    button.grid(row=i, column=0, pady=5)
-    button.configure(command=lambda mode=button_info["mode"]: on_button_click(mode))
+    button = customtkinter.CTkButton(canvas, image=img, fg_color="#FFFFFF", text=button_info["text"], text_color="#000", hover_color="#FFF")
+    button_widgets.append(button)
+    button.bind("<Button-1>", lambda event, mode=button_info["mode"]: on_button_click(mode))
+    button.place(x=10, y=10 + i * 60)  # Adjust the placement as needed
 
-
-canvas = customtkinter.CTkCanvas(root, bg="white", width=750, height=600)
-canvas.place(x=100, y=10)
-canvas.tk_img = ImageTk.PhotoImage(image=Image.fromarray(cv2.cvtColor(canvas_image, cv2.COLOR_BGRA2RGBA)))
-canvas.canvas_item = canvas.create_image(0, 0, anchor="nw", image=canvas.tk_img)
-canvas.bind("<Button-1>", on_canvas_click)
-canvas.bind("<B1-Motion>", on_canvas_drag)
-canvas.bind("<ButtonRelease-1>", on_canvas_release) 
 root.mainloop()
